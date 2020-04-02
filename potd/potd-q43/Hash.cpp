854d725677f9e290be840fdd9479d4ff7a89f895
Taoyu Cai <taoyuc3@illinois.edu>
2020-04-02 14:36:34 -0500

    potd-q43 finished

---
+++ potd/potd-q43/Hash.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include "Hash.h"
+
+unsigned long bernstein(std::string str, int M)
+{
+	unsigned long b_hash = 5381;
+	for (int i = 0; i < (int) str.length(); ++i)
+	{
+		b_hash = b_hash * 33 + str[i];
+	}
+	return b_hash % M;
+}
+
+float hash_goodness(std::string str, int M)
+{
+  std::vector<int>* array = new std::vector<int>(M);	// Hint: This comes in handy
+	int permutation_count = 0;
+	float goodness = 0;
+	float collisions = 0;
+	do {
+		if (permutation_count == M) break;
+		// Code for computing the hash and updating the array
+		int key = bernstein(str, M);
+		(*array)[key]++;
+		//update count
+		permutation_count ++;
+	} while(std::next_permutation(str.begin(), str.end()));
+
+	//Code for determining goodness
+	for(auto elem: *array){
+		if(elem > 1	){
+			collisions += elem-1;
+		}
+	}
+	goodness = collisions / M;
+	return goodness;
+}
